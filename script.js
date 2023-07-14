8413b064b4994115723400f1fcb8a2188d418640
VAISHNAVI KAWALE <114976538+vais2004@users.noreply.github.com>
2023-07-14 18:44:52 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputOne = document.querySelector("#input-1")
 const inputTwo = document.querySelector("#input-2")
 const plusButton = document.querySelector("#plus")
-const minusButton = document.querySelector("minus")
+const minusButton = document.querySelector("#minus")
 const multiplyButton = document.querySelector("#multiply")
 const divideButton = document.querySelector("#divide")
 const output = document.querySelector("#output")
@@ -16,11 +16,11 @@ function multiplyHandler(){
     output.innerText = Number(inputOne.value) * Number(inputTwo.value)
 }
 function divideHandler(){
-    output.innerText = Number(inputOnej.value) / Number(inputTwo.value)
+    output.innerText = Number(inputOne.value) / Number(inputTwo.value)
 }
 
 plusButton.addEventListener('click', plusHandler)
-minusButton.addEventListener('click', minusButton)
+minusButton.addEventListener('click', minusHandler)
 multiplyButton.addEventListener('click', multiplyHandler)
 divideButton.addEventListener('click', divideHandler)
 
